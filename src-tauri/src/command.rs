21efd5f97783ced8b3ec290e9458650f87ad4bb1
Selenium39 <895484122@qq.com>
2022-09-04 18:46:26 +0800

    下载该用户所有视频

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -1,6 +1,7 @@
 use regex::Regex;
 
 #[derive(serde::Serialize)]
+#[derive(Debug)]
 pub struct VideoInfo{
     title:String,
     ratio:String,
@@ -10,56 +11,84 @@ pub struct VideoInfo{
 }
 
 #[tauri::command]
-pub async fn parse_dy(url: &str) ->  Result<VideoInfo, String> {
-    // 获请求抖音链接中的url
-    let short_url = Regex::new(r"https://\S*")
-        .unwrap()
-        .captures(url)
-        .unwrap()
-        .get(0)
-        .unwrap()
-        .as_str()
-        .to_string();
-    // 请获取视频唯一id
-    let video_url = reqwest::get(&short_url)
-        .await
-        .unwrap()
-        .url()
-        .as_str()
-        .to_string();
-    let video_id = Regex::new(r"video/(\d+)")
-        .unwrap()
-        .captures(&video_url)
-        .unwrap()
-        .get(1)
-        .unwrap()
-        .as_str()
-        .to_string();
-    // 获取视频播放地址
-    let resp = reqwest::get("https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=".to_string() + video_id.as_str())
+pub async fn parse_dy(url: &str,parse_type:&str) ->  Result<Vec<VideoInfo>, String> {
+    let mut videos = Vec::new();
+    if parse_type=="video"{
+        // 获请求抖音链接中的url
+        let short_url = Regex::new(r"https://\S*")
+            .unwrap()
+            .captures(url)
+            .unwrap()
+            .get(0)
+            .unwrap()
+            .as_str()
+            .to_string();
+        // 请获取视频唯一id
+        let video_url = reqwest::get(&short_url)
             .await
             .unwrap()
-            .json::<serde_json::Value>()
-            .await
-            .unwrap();
-    let video_obj = &resp["item_list"][0];
-    let video_url = video_obj["video"]["play_addr"]["url_list"][0].as_str().unwrap().to_string();
-    // 获取去水印视频地址
-    let video_no_wm_url = video_url.replace("playwm", "play");
-    Ok(VideoInfo {
-        title: video_obj["desc"]
+            .url()
             .as_str()
-            .unwrap_or("")
-            .to_string(),
-        ratio: video_obj["video"]["ratio"]
+            .to_string();
+        let video_id = Regex::new(r"video/(\d+)")
+            .unwrap()
+            .captures(&video_url)
+            .unwrap()
+            .get(1)
+            .unwrap()
             .as_str()
-            .unwrap_or("")
-            .to_string(),
-        cover: video_obj["video"]["cover"]["url_list"][0]
-            .as_str()
-            .unwrap_or("")
-            .to_string(),
-        id: video_obj["aweme_id"].as_str().unwrap_or("").to_string(),
-        url:video_no_wm_url,
-    })
+            .to_string();
+        // 获取视频播放地址
+        let resp = reqwest::get("https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=".to_string() + video_id.as_str())
+                .await
+                .unwrap()
+                .json::<serde_json::Value>()
+                .await
+                .unwrap();
+        let video_obj = resp["item_list"][0].clone();
+        videos.push(video_obj);
+    }else{
+       // 获取用户uid
+       let uid= Regex::new(r#"https://www.douyin.com/user/([\w-]+)"#).unwrap().captures(url).unwrap().get(1).unwrap().as_str().to_string();
+       let raw_info = reqwest::get("https://www.iesdouyin.com/web/api/v2/user/info/?sec_uid=".to_string()+uid.as_str())
+                        .await
+                        .unwrap()
+                        .json::<serde_json::Value>()
+                        .await
+                        .unwrap();
+        let video_count = raw_info["user_info"]["aweme_count"].as_u64().unwrap();
+        // 获取用户视频列表
+        let raw_videos_info = reqwest::get(format!("https://www.iesdouyin.com/web/api/v2/aweme/post/?sec_uid={}&count={}&max_cursor=0",uid,video_count))
+        .await
+        .unwrap()
+        .json::<serde_json::Value>()
+        .await
+        .unwrap();
+        let video_objs = raw_videos_info["aweme_list"].as_array().unwrap().clone();
+        for video_obj in video_objs{
+            videos.push(video_obj);
+        }
+    }
+    let result = videos.iter().map(|video_obj|{
+            let video_url = video_obj["video"]["play_addr"]["url_list"][0].as_str().unwrap().to_string();
+            // 获取去水印视频地址
+            let video_no_wm_url = video_url.replace("playwm", "play");
+            VideoInfo {
+                title: video_obj["desc"]
+                    .as_str()
+                    .unwrap_or("")
+                    .to_string(),
+                ratio: video_obj["video"]["ratio"]
+                    .as_str()
+                    .unwrap_or("")
+                    .to_string(),
+                cover: video_obj["video"]["cover"]["url_list"][0]
+                    .as_str()
+                    .unwrap_or("")
+                    .to_string(),
+                id: video_obj["aweme_id"].as_str().unwrap_or("").to_string(),
+                url:video_no_wm_url,
+            }
+    }).collect();
+    Ok(result)
 }
--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <div id="form">
       <el-row style="padding: 20px 0" type="flex" justify="space-around">
         <el-col :span="5">
-          <el-select style="width: 100%" v-model="form.type">
-            <el-option label="单个视频" value="single"></el-option>
-            <el-option label="用户所有视频" value="multi"></el-option>
+          <el-select style="width: 100%" v-model="form.parseType">
+            <el-option label="单个视频" value="video"></el-option>
+            <el-option label="用户所有视频" value="user"></el-option>
           </el-select>
         </el-col>
         <el-col :span="15">
@@ -43,8 +43,8 @@
         <el-table-column label="操作" align="center">
           <template slot-scope="scope">
             <el-button
-              @click="download(scope.row.url,scope.row.title)"
-              :loading="downloading"
+              @click="download(scope.row)"
+              :loading="scope.row.downloading"
               type="primary"
               size="small"
               ><i class="el-icon-download"></i>下载</el-button
@@ -64,30 +64,33 @@ export default {
   data() {
     return {
       form: {
-        type: "single",
-        url: "8.25 CHI:/ look 别发呆 %看我造型say哇塞  https://v.douyin.com/Fpv7GVp/ 复制此链接，打开Dou音搜索，直接观看视频！",
+        parseType: "video",
+        // url: "8.25 CHI:/ look 别发呆 %看我造型say哇塞  https://v.douyin.com/Fpv7GVp/ 复制此链接，打开Dou音搜索，直接观看视频！",
+        url:"https://www.douyin.com/user/MS4wLjABAAAACrxUoQvX2JVH3NeVKvErEEG59BuU5hs_Z-rH9dP0tH4"
       },
-      list: [],
-      downloading:false
+      list: []
     };
   },
   methods: {
     parse() {
       invoke("parse_dy", this.form).then((res) => {
-        this.list = [res];
+        res.forEach(item=>{
+          item.downloading = false
+        })
+        this.list = res;
       });
     },
-    async download(url,title) {
-      this.downloading = true
-      const res = await fetch(url, {
+    async download(video) {
+      video.downloading=true
+      const res = await fetch(video.url, {
         method: "GET",
         timeout: 10000,
         responseType: ResponseType.Binary,
       }); 
-      await writeBinaryFile(`${title}.mp4`, res.data,{dir:BaseDirectory.Download})
-      this.downloading = false
+      await writeBinaryFile(`${video.id}.mp4`, res.data,{dir:BaseDirectory.Download})
+      video.downloading = false
       this.$message({
-          message: `视频已下载至${BaseDirectory.Download}`,
+          message: '视频已下载',
           type: 'success'
       });
     },
